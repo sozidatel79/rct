f68c5ae910e5a9ecdf1c960ca0f203e1da067096
Anton <sozidatel79>
2017-05-03 13:19:27 +0300

    import fixes

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Hello = require('./components/Hello');
-var obj1 = {
-    name:'Anton',
-    age:37
-}
-var obj2 = {
-    sity:'Holon',
-    street:'Hageonim',
-    ...obj1
-}
-
-console.log(obj2);
 ReactDOM.render(
     <Hello/>,
     document.querySelector('#app')
